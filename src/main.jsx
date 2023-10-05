a4a68d5935d36eb6afada5ec69a53bf4de0b7cc9
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-10-05 17:17:37 +0300

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom/client";
-import Router from "./routes/Router";
+import Router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Router />);
